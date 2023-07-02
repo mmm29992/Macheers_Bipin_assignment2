c38175be84a8ba00fa01f74291d2bc88220d3c2c
michael <michaelmacheers@Michaels-MacBook-Pro-2.local>
2023-07-01 21:09:13 -0400

    insert method -michael

---
+++ DoublyLinkedList.java
@@ -0,0 +1,65 @@
+import NodeType;
+
+public class DoublyLinkedList<T extends Comparable<T>> {
+    private NodeType<T> head;
+
+    /**
+     * Constructs an empty DoublyLinkedList
+     * The constructor initializes the head of the list to null, indicating an empty
+     * list.
+     * This constructor is used to create a new instance of the DoublyLinkedList
+     * class.
+     * The head variable will be updated when elements are added to the list.
+     * 
+     * @param <T> the type of elements held in this DoublyLinkedlist
+     */
+    DoublyLinkedList() {
+        head = null;
+    }
+
+    public void insertItem( T item) 
+    {
+        NodeType<T> newNode = new NodeType<>();
+        NodeType<T> temp = head;
+        newNode.info = item;
+
+        // if list is empty
+        if (head == null) 
+        {
+            head = newNode;
+        } else if (newNode.info.compareTo(head.info) < 0)   // insert at the beginning 
+        {
+            newNode.next = head;
+            head.back = newNode;
+            head = newNode;
+        } else  // insert in the middle
+        {
+while (item.compareTo(temp.info) > 0)
+{
+    if (temp.next != null) {
+        temp = temp.next;
+    } else
+        break;
+}
+if ( temp.next == null && newNode.info.compareTo(temp.info) > 0)
+{
+    temp.next = newNode;
+    newNode.back = temp;
+} else 
+{
+    NodeType<T> back = temp.back;
+    newNode.next = temp;
+    temp.back = newNode;
+    newNode.back = back;
+    back.next = newNode;
+}
+
+        }
+
+       
+        
+
+
+    }
+
+}
---
+++ NodeType.java
@@ -0,0 +1,5 @@
+public class NodeType<T extends Comparable<T>> {
+public T info;
+public NodeType<T> next;
+public NodeType<T> back;
+} ;
